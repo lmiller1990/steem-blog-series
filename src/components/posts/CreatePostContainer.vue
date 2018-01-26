554570a0f0fe421c48c218217fe52bf7a60e98af
lachlan <lachlan.miller.1990@outlook.com>
2018-01-26 22:45:12 +0900

    Update and test:

--- src/components/posts/CreatePostContainer.vue
+++ src/components/posts/CreatePostContainer.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script>
-  import steem as 'steem'
+  import steem from 'steem'
   import CreatePostLayout from './CreatePostLayout'
   import CreateDescription from './CreateDescription'
   import SubmitButton from '@/components/shared/SubmitButton'
@@ -35,11 +35,27 @@
       }
     },
 
+    created () {
+      steem.api.setOptions({ url: 'https://api.steemit.com' })
+    },
+
     methods: {
       handleCreatePost () {
-        console.log('submitted')
+        const key = ''
         steem.broadcast.comment(
-
+          key,
+          '',
+          'test',
+          'xenetics',
+          'instagram-clone-steem',
+          'A test post for creating a Instagram clone',
+          'This is a test post for a series about creating a Instgram like website on the Steem blockchain. More here: steemit.com/@xenetics',
+          { tags: ['test', 'test-2', 'test-3'] },
+          (err, result) => {
+            if (!err) {
+              console.log('Result', result)
+            }
+          }
         )
       }
     }
